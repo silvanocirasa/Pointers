ddf572a6b979da89252f298b254f4a02d3461452
Sil Vo <cirasa.silvano@gmail.com>
2021-03-26 18:19:22 +0000

    create function that doubles the values in array

--- main.c
+++ main.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 
-int SumOfElements(int *A, int size) // "int * A " and "int A[]" is the same
+int Double(int *A, int size) // "int * A " and "int A[]" is the same
 {
   int i, sum = 0;
-  printf("SOE - Size of A = %d, size of A[0] = %d\n", sizeof(A), sizeof(A[0]));
   for(i = 0; i < size;i++)
   {
-    sum+= A[i];
-  }
-  return sum;
+    A[i] = 2*A[i];
+}
 }
 int main(void)
 {
   int A[] = {1,2,3,4,5};
   int size = sizeof(A)/sizeof(A[0]);
-  int total = SumOfElements(A,size);
-  printf("Sum of Elements = %d\n", total);
-  printf("Main - Size of A = %d, size of A[0] = %d\n", sizeof(A),sizeof(A[0])); 
+  int i;
+  Double(A, size);
+  for(i = 0; i < size; i++)
+  {
+    printf("%d \n", A[i]);
+  }
 }
 
